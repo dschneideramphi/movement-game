ad843ebefba20a3f7d99186bccd320dfab4069e1
dschneideramphi <dschneider@amphi.com>
2019-09-17 17:38:09 -0700

    Added hero on load

--- MyWorld.java
+++ MyWorld.java
@@ -16,6 +16,8 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(500, 500, 1); 
+        super(500, 500, 1);
+        Hero h = new Hero();
+        addObject(h, 250, 250);
     }
 }
