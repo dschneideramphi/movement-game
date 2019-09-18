3a48db5b7c051ca047c79f3331db2fc89cea41f7
dschneideramphi <dschneider@amphi.com>
2019-09-17 17:51:55 -0700

    velocity movement

--- Hero.java
+++ Hero.java
@@ -3,8 +3,36 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Hero extends Actor
 {
 
+    private int Vx = 0;
+    private int Vy = 0;
+    
     public void act() 
     {
+        int x = getX();
+        int y = getY();
+        if(Greenfoot.isKeyDown("left")) {
+            Vx -= 1;
+            setImage("left.png");
+        }
         
+        if(Greenfoot.isKeyDown("right")) {
+            Vx += 1;
+            setImage("right.png");
+        }
+        
+        if(Greenfoot.isKeyDown("up")) {
+            Vy -= 1;
+            setImage("up.png");
+        }
+        
+        if(Greenfoot.isKeyDown("down")) {
+            Vy += 1;
+            setImage("down.png");
+        }
+        
+        x += Vx;
+        y += Vy;
+        
+        setLocation(x, y);
     }    
 }
