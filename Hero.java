bbf10c5710a78cefe39c98795d2b1d3ff37cb87d
dschneideramphi <dschneider@amphi.com>
2019-09-19 12:53:11 -0700

    in-class work

--- Hero.java
+++ Hero.java
@@ -5,6 +5,33 @@ public class Hero extends Actor
 
     public void act() 
     {
+        int x = getX();
+        int y = getY();
         
+        if(Greenfoot.isKeyDown("up")) {
+            y = y - 5;
+            setImage("up.png");
+
+        }
+        
+        if(Greenfoot.isKeyDown("down")) {
+            y = y + 5;
+            setImage("down.png");
+
+        }
+        
+        if(Greenfoot.isKeyDown("left")) {
+            x = x - 5;
+            setImage("left.png");
+
+        }
+        
+        if(Greenfoot.isKeyDown("right")) {
+            x = x + 5;
+            setImage("right.png");
+        }
+        
+        
+        setLocation(x, y);
     }    
 }
