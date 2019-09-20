488ac964fe303b8f6b34527f1019d9bdab2a23cb
dschneideramphi <dschneider@amphi.com>
2019-09-20 14:30:04 -0700

    updated

--- Hero.java
+++ Hero.java
@@ -1,36 +1,65 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-public class Hero extends Actor
+public class Hero extends SmoothMover
 {
 
+    private double Vx = 0;
+    private double Vy = 0;
+    private double speed = 5;
+    
     public void act() 
     {
-        int x = getX();
-        int y = getY();
+        
+        
+        double x = getExactX();
+        double y = getExactY();
         
         if(Greenfoot.isKeyDown("up")) {
-            y = y - 5;
+            if(Vy >= -1*speed) {
+                Vy -= 0.2;
+            }
             setImage("up.png");
-
+            
         }
         
         if(Greenfoot.isKeyDown("down")) {
-            y = y + 5;
+            if(Vy <= speed) {
+                Vy += 0.2;
+            }
             setImage("down.png");
 
         }
         
         if(Greenfoot.isKeyDown("left")) {
-            x = x - 5;
+            if(Vx > -1*speed) {
+                Vx -= 0.2;
+            }
             setImage("left.png");
 
         }
         
         if(Greenfoot.isKeyDown("right")) {
-            x = x + 5;
+            if(Vx < speed) {
+                Vx += 0.2;
+            }
             setImage("right.png");
         }
         
+        x += Vx;
+        y += Vy;
+        
+        if(x > 500) {
+            x = 0;
+        }
+        if(x < 0) {
+            x = 500;
+        }
+        if(y > 500) {
+            y = 0;
+        }
+        if(y < 0) {
+            y = 500;
+        }
         
         setLocation(x, y);
     }    
--- MyWorld.java
+++ MyWorld.java
@@ -16,7 +16,7 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(500, 500, 1);
+        super(500, 500, 1, false);
         Hero h = new Hero();
         addObject(h, 250, 250);
     }
